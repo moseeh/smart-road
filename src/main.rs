a036595eaabf78367c9d0056ee1a2b721ce5e532
moonyango <mosesadrian825@gmail.com>
2025-07-27 06:17:15 +0300

    fix: add missing vehicle module declaration

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use sdl2::event::Event;
 use sdl2::image::{InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
+mod vehicle;
 
 fn main() -> Result<(), String> {
     // Initialize SDL2
