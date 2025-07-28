ab71f91b41384f62ccb0a9147ace95109bca27cd
moonyango <mosesadrian825@gmail.com>
2025-07-28 16:05:05 +0300

    feat: refactor Vehicle struct and constructor to include route, direction, and speed attributes

--- src/vehicle.rs
+++ src/vehicle.rs
@@ -1,22 +1,39 @@
+use crate::route::{Direction, Route};
+use crate::velocities::Velocity;
 use rand::Rng;
 use sdl2::image::LoadTexture;
 use sdl2::render::{Texture, TextureCreator};
 use sdl2::video::WindowContext;
 
-
-pub struct VehicleTexture<'a> {
+pub struct Vehicle<'a> {
     pub texture: Texture<'a>,
+    pub route: Route,
+    pub direction: Direction,
+    pub current_speed: Velocity,
+    pub width: u32,
+    pub height: u32,
+    pub safety_distance: f32,
 }
 
-impl<'a> VehicleTexture<'a> {
-    pub fn new(texture_creator: &'a TextureCreator<WindowContext>) -> Result<Self, String> {
-        // Pick a random number from 1 to 5
+impl<'a> Vehicle<'a> {
+    pub fn new(
+        texture_creator: &'a TextureCreator<WindowContext>,
+        route: Route,
+        direction: Direction,
+    ) -> Result<Self, String> {
         let mut rng = rand::rng();
-        let car_index = rng.random_range(1..=5); // inclusive range
-
+        let car_index = rng.random_range(1..=5);
         let path = format!("assets/Cars/car{}.png", car_index);
         let texture = texture_creator.load_texture(&path)?;
 
-        Ok(Self { texture })
+        Ok(Self {
+            texture,
+            route,
+            direction,
+            current_speed: Velocity::Medium,
+            width: 60,
+            height: 30,
+            safety_distance: 50.0,
+        })
     }
 }
