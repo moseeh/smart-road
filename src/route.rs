b60dbb52cbded6ca697983e45f62cd8b50e29282
moonyango <mosesadrian825@gmail.com>
2025-07-29 22:31:50 +0300

    feat: helper function to get a random route

--- src/route.rs
+++ src/route.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Route {
     Right,
@@ -11,4 +13,14 @@ pub enum Direction {
     South, // Coming from north, going south
     East,  // Coming from west, going east
     West,  // Coming from east, going west
+}
+
+// Helper function to get random route
+pub fn get_random_route() -> Route {
+    let mut rng = rand::rng();
+    match rng.random_range(0..3) {
+        0 => Route::Right,
+        1 => Route::Straight,
+        _ => Route::Left,
+    }
 }
