1215d92efc71af9ca456b819bac69ffad072e1db
moonyango <mosesadrian825@gmail.com>
2025-07-29 22:00:56 +0300

    refactor:add traits to structs

--- src/route.rs
+++ src/route.rs
@@ -1,12 +1,14 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Route {
     Right,
     Left,
     Straight,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Direction {
-    North,  // Coming from south, going north
-    South,  // Coming from north, going south  
-    East,   // Coming from west, going east
-    West,   // Coming from east, going west
+    North, // Coming from south, going north
+    South, // Coming from north, going south
+    East,  // Coming from west, going east
+    West,  // Coming from east, going west
 }
