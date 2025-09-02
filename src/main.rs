e9b0b0ec1e96d94490c4eeb48c09aa1147c03b65
moonyango <mosesadrian825@gmail.com>
2025-09-02 04:11:53 +0300

    fix: update average stats display

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use sdl2::event::Event;
 use sdl2::image::{InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
-use sdl2::rect::Rect;
 use std::time::Duration;
 mod intersection;
 mod route;
 mod vehicle;
+mod stats;
 mod velocities;
 
 use intersection::*;
 use route::*;
+use stats::*;
+
 
 // Constants for the game design
 const WINDOW_WIDTH: u32 = 1000;
@@ -122,82 +123,6 @@ fn run_game(
         canvas.present();
         std::thread::sleep(FRAME_DELAY);
     }
-}
-
-fn show_stats(
-    sdl_context: &sdl2::Sdl,
-    video_subsystem: &sdl2::VideoSubsystem,
-    ttf_context: &sdl2::ttf::Sdl2TtfContext,
-    stats_text: &str,
-) -> Result<(), String> {
-    let window = video_subsystem
-        .window("Statistics", 1000, 1000)
-        .position_centered()
-        .build()
-        .map_err(|e| e.to_string())?;
-
-    let mut canvas = window
-        .into_canvas()
-        .present_vsync()
-        .build()
-        .map_err(|e| e.to_string())?;
-
-    let texture_creator = canvas.texture_creator();
-    let font = ttf_context.load_font("assets/fonts/OpenSans-Bold.ttf", 24)?;
-
-    let mut event_pump = sdl_context.event_pump()?;
-
-    let lines: Vec<&str> = stats_text.split('\n').collect();
-    let mut y = 50;
-
-    'stats_running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => {
-                    break 'stats_running;
-                }
-                _ => {}
-            }
-        }
-
-        canvas.set_draw_color(Color::RGB(0, 255, 255));
-        canvas.clear();
-
-        for line in &lines {
-            if line.is_empty() {
-                y += 10; // Add some space for empty lines
-                continue;
-            }
-            let surface = font
-                .render(line)
-                .shaded(Color::RGBA(0, 0, 0, 255), Color::RGBA(0, 255, 255, 255))
-                .map_err(|e| e.to_string())?;
-
-            let texture = texture_creator
-                .create_texture_from_surface(&surface)
-                .map_err(|e| e.to_string())?;
-
-            let query = texture.query();
-            let target_rect = Rect::new(
-                (1000 - query.width as i32) / 2,
-                y,
-                query.width,
-                query.height,
-            );
-            canvas.copy(&texture, None, target_rect)?;
-            y += query.height as i32 + 5; // Move y for the next line
-        }
-        y = 50; // Reset y for the next frame
-
-        canvas.present();
-        std::thread::sleep(FRAME_DELAY);
-    }
-
-    Ok(())
 }
 
 fn main() -> Result<(), String> {
